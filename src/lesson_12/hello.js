8198048705b13e879f30f8136ca3a982e81dde07
Illia Lytvynov <ilyaillych@gmail.com>
2020-01-18 15:16:58 +0200

    Lesson 13

---
+++ src/lesson_12/hello.js
@@ -0,0 +1,38 @@
+const red = document.querySelector('.red');
+const yellow = document.querySelector('.yellow');
+const green = document.querySelector('.green');
+const btnStart = document.getElementById('start');
+const btnStop = document.getElementById('stop');
+const toggleOff = () => {
+  red.style.backgroundColor = '';
+  yellow.style.backgroundColor = '';
+  green.style.backgroundColor = '';
+  btnStart.disabled = true; // блокируем кнопку Старт
+  btnStop.disabled = false;
+};
+let isEnabled = false;
+
+red.onclick = function() {
+  if (isEnabled === true) {
+    toggleOff();
+    red.style.backgroundColor = 'red';
+  }
+};
+yellow.onclick = function() {
+  if (isEnabled === true) {
+    toggleOff();
+    yellow.style.backgroundColor = 'yellow';
+  }
+};
+green.onclick = function() {
+  if (isEnabled === true) {
+    toggleOff();
+    green.style.backgroundColor = 'green';
+  }
+};
+
+const toggle = () => {
+  isEnabled = !isEnabled;
+};
+btnStart.addEventListener('click', toggle);
+btnStop.addEventListener('click', toggle);
