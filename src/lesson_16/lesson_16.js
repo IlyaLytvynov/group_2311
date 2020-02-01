c322c6e1c2161a8aaa1c0e6e698860c99f992750
Illia Lytvynov <ilyaillych@gmail.com>
2020-02-01 11:48:21 +0200

    lesson 16

---
+++ src/lesson_16/lesson_16.js
@@ -0,0 +1,17 @@
+import './lesson_16.scss';
+import { Feed } from './feed';
+const body = document.querySelector('body');
+const feed = new Feed(body);
+feed.init();
+
+const xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'http://localhost:3000/movies');
+xhr.send();
+
+xhr.onload = () => {
+  if (xhr.status >= 200 && xhr.status < 400) {
+    const data = JSON.parse(xhr.response);
+    console.log(data);
+  }
+};
